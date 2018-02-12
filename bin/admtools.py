94eed2277db1bdbe5db1134026fd3d30ac8f30cf
AlexeyVAV <vitkalovav@gmail.com>
2018-02-12 15:05:19 +0000

    2018-02-12 2

--- bin/admtools.py
+++ bin/admtools.py
@@ -2,7 +2,7 @@ import paramiko
 
 hostname = ''
 port = 22
-username = 'root'
+username = 'oracle'
 password = ''
 
 if __name__ == '__main__':
@@ -11,9 +11,10 @@ if __name__ == '__main__':
     s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     s.load_system_host_keys()
     s.connect(hostname, port, username, password)
-    stdin, stdout, stderr = s.exec_command('ifconfig')
-    print stdout.read()
     stdin, stdout, stderr = s.exec_command('hostname -s')
+    print stdout.read()
+    stdin, stdout, stderr = s.exec_command('export ORACLE_HOME=/u01/app/oracle/product/12.1.0.2/dbhome_1; export PATH=$PATH:$ORACLE_HOME/bin; srvctl status database -d gbtukts1 2>&1')
+    #print stdin
     print stdout.read()
     s.close()
 
